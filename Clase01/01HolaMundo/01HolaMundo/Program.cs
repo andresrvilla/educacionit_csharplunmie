e78e6af294216e7d2b3337793b6c2d94283e6510
Roberto Andrés Villa <mailglobal@gmail.com>
2021-12-13 20:00:17 -0300

    Clase 1

---
+++ Clase01/01HolaMundo/01HolaMundo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01HolaMundo
+{
+    internal class Program
+    {
+        //http://github.com/andresrvilla/educacionit_csharplunmie
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            // Definicion de variable
+            /*
+             * TIPO NOMBRE;
+             */
+            string denominacion;
+
+            // denominacion = "Andres";
+
+            Console.WriteLine("Por favor ingrese su nombre: ");
+
+            denominacion = Console.ReadLine();
+
+            Console.WriteLine("Hola " + denominacion);
+
+            Console.ReadLine();
+        }
+    }
+}
